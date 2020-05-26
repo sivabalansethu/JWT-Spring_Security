fe667f5eebc3883fb8eb935d70a31861826d2112
sivabalan.sethu@gmail.com <sivabalan.sethu@gmail.com>
2020-05-26 16:36:28 +0530

    Added Newly

---
+++ src/main/java/com/agritech/security/jwtsecurity/security/JwtGenerator.java
@@ -0,0 +1,28 @@
+package com.agritech.security.jwtsecurity.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import com.agritech.security.jwtsecurity.model.JwtUser;
+
+@Component
+public class JwtGenerator {
+
+
+    public String generate(JwtUser jwtUser) {
+
+
+        Claims claims = Jwts.claims()
+                .setSubject(jwtUser.getUserName());
+        claims.put("userId", String.valueOf(jwtUser.getId()));
+        claims.put("role", jwtUser.getRole());
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, "youtube")
+                .compact();
+    }
+}
